6b635d56251e1e980bba762ca0960499ece906ea
Saty-aNarayan <satyanarayan.jaganathan2001@gmail.com>
2026-06-27 00:12:36 +0530

    working solution

---
+++ LogAchiveTool/archive.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FileName="$1"
+echo "FileName: $FileName"
+DATE=$(date +"%Y%m%d_%H%M%S")
+echo "DATE: $DATE"
+name=${FileName}_${DATE}
+echo "Archiving the file: $name"
+tar -czf "$name.tar.gz" "$name"
+echo "File archived successfully: $name.tar.gz"
